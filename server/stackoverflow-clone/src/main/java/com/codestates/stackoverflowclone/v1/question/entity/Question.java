3e70beea262f42f7888443921e4f84fd93a5c6e7
sungmingt <qotjdals8313@naver.com>
2022-08-29 14:50:32 +0900

    220829 질문 등록/수정/조회 api

--- server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/question/entity/Question.java
+++ server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/question/entity/Question.java
@@ -1,32 +1,14 @@
-
 package com.codestates.stackoverflowclone.v1.question.entity;
 
 import com.codestates.stackoverflowclone.v1.answer.Answer;
 import com.codestates.stackoverflowclone.v1.audit.Auditable;
 import com.codestates.stackoverflowclone.v1.member.entity.Member;
-import com.codestates.stackoverflowclone.v1.tag.Tag;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static javax.persistence.CascadeType.*;
-import static javax.persistence.FetchType.*;
-
-package com.codestates.stackoverflowclone.v1.question.entity;
-
-import com.codestates.stackoverflowclone.v1.answer.Answer;
-import com.codestates.stackoverflowclone.v1.audit.Auditable;
-import com.codestates.stackoverflowclone.v1.member.entity.Member;
-import com.codestates.stackoverflowclone.v1.tag.Tag;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import static javax.persistence.CascadeType.*;
@@ -34,8 +16,6 @@ import static javax.persistence.FetchType.*;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
-import java.util.List;
 
 import static javax.persistence.GenerationType.*;
 
@@ -53,19 +33,18 @@ public class Question extends Auditable {
     private String title;
     private String content;
     private int viewCount;
-
     private int answerCount; ///
-
-    private int answerCount ///
 
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")  
     private Member member;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "question", orphanRemoval = true)
     private List<Answer> answers = new ArrayList<>();
 
+    @JsonIgnore
     @OneToMany(mappedBy = "question", cascade = PERSIST, orphanRemoval = true)
     private List<QuestionTag> questionTags = new ArrayList<>();
 
@@ -101,7 +80,7 @@ public class Question extends Auditable {
 
     public void setMember(Member member) {
         this.member = member;
-//        member.getQuestions.add(this);
+        member.getQuestions().add(this);
     }
     public void addQuestionTag(QuestionTag questionTag) {
         questionTags.add(questionTag);
--- server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/question/repository/QuestionRepository.java
+++ server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/question/repository/QuestionRepository.java
@@ -1,16 +1,10 @@
 package com.codestates.stackoverflowclone.v1.question.repository;
 
 import com.codestates.stackoverflowclone.v1.question.entity.Question;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
-
-public interface QuestionRepository extends JpaRepository<Question, Integer> {
-
-    List<Question> findByContentContains(String content);
-
-}
 
 public interface QuestionRepository extends JpaRepository<Question, Integer>, QuestionCustomRepository {
 
