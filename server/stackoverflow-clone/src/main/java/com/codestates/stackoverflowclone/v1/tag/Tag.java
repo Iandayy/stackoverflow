fa3b9e761d206418d0f75935ddad97428b6224fb
sungmingt <qotjdals8313@naver.com>
2022-08-29 09:34:00 +0900

    220829 질문 구현 상황공유 커밋

---
+++ server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/tag/Tag.java
@@ -0,0 +1,45 @@
+package com.codestates.stackoverflowclone.v1.tag;
+
+import com.codestates.stackoverflowclone.v1.question.entity.QuestionTag;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.GenerationType.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Tag {
+
+    @Id
+    private String name;
+
+    private int usageCount;
+
+    @OneToMany(mappedBy = "tag", orphanRemoval = true)
+    private List<QuestionTag> questionTags = new ArrayList<>();
+
+
+    public Tag(String name) {
+        this.name = name;
+    }
+    public Tag(String name, int usageCount) {
+        this.name = name;
+        this.usageCount = usageCount;
+    }
+
+
+    //===================
+
+    public void addQuestionTag(QuestionTag questionTag) {
+        questionTags.add(questionTag);
+        questionTag.setTag(this);
+    }
+    public void addUsageCount() {
+        this.usageCount++;
+    }
+}
