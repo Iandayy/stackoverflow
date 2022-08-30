21394b1faa9fbbcc5d50b64685583d248fb6231c
Damgom <wisewish1@gmail.com>
2022-08-30 15:10:49 +0900

    add CorsConfig

---
+++ server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.codestates.stackoverflowclone.v1.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedHeaders("GET","POST");
+    }
+}
