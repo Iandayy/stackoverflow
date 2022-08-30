564e7246a7318490b83c0640ef6609871b8a7ea7
Damgom <wisewish1@gmail.com>
2022-08-30 15:52:00 +0900

    modify webconfig

--- server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/config/WebConfig.java
+++ server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedHeaders("GET","POST");
+                .allowedMethods("GET","POST");
     }
 }
