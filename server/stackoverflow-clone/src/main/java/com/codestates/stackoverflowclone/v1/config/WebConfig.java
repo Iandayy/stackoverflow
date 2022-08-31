b236b1ee0a584f1cb18cbcb75452391643e6d3d1
Damgom <wisewish1@gmail.com>
2022-08-31 15:57:41 +0900

    220831 expose Authorization

--- server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/config/WebConfig.java
+++ server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/config/WebConfig.java
@@ -10,7 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET","POST");
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowedMethods("GET","POST")
+                .maxAge(3600)
+                .allowCredentials(false);
     }
 }
