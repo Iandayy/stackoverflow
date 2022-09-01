daab9dd8567afaed9b7fe3e14c15a6816beb0666
Damgom <wisewish1@gmail.com>
2022-09-01 15:57:46 +0900

    220901 addHeader memberid

--- server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/config/CorsConfig.java
+++ server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/config/CorsConfig.java
@@ -18,6 +18,7 @@ public class CorsConfig {
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("Authorization");
+        config.addExposedHeader("member_id");
         source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
