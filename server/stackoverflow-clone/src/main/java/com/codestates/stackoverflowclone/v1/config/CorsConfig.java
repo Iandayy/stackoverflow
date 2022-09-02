b1980a8934f77f64277f5a62b45cee1026d7b90a
Damgom <wisewish1@gmail.com>
2022-09-02 16:27:12 +0900

    220902 modified mapper

--- server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/config/CorsConfig.java
+++ server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/config/CorsConfig.java
@@ -13,8 +13,8 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(false);
-        config.addAllowedOrigin("*");
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("Authorization");
--- server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/member/mapper/MemberMapper.java
+++ server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/member/mapper/MemberMapper.java
@@ -14,7 +14,25 @@ public interface MemberMapper {
 
     Member memberPatchToMember(MemberDto.Patch requestBody);
 
-    MemberDto.Response memberToMemberResponse(Member member);
+
+    default MemberDto.Response memberToMemberResponse(Member member){
+        if( member == null){
+            return null;
+        }
+
+        String email = null;
+        String name = null;
+        int memberId = 0;
+
+        email = member.getEmail();
+        name = member.getName();
+        memberId = member.getId();
+
+        int memberID = 0;
+
+        MemberDto.Response response = new MemberDto.Response(memberId, email, name);
+        return response;
+    }
 
     List<MemberDto.Response> memberToMemberResponses(List<Member> members);
 }
