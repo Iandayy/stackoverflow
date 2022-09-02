a6f7b20fc1698b9bb5512630e4907fea00383d5a
Damgom <wisewish1@gmail.com>
2022-09-02 09:23:02 +0900

    220902 modified corsconfig

--- server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/config/WebConfig.java
+++ server/stackoverflow-clone/src/main/java/com/codestates/stackoverflowclone/v1/config/WebConfig.java
@@ -1,20 +1,20 @@
-package com.codestates.stackoverflowclone.v1.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .exposedHeaders("*")
-                .allowedMethods("GET","POST")
-                .maxAge(3600)
-                .allowCredentials(false);
-    }
-}
+//package com.codestates.stackoverflowclone.v1.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry){
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedHeaders("*")
+//                .exposedHeaders("*")
+//                .allowedMethods("GET","POST")
+//                .maxAge(3600)
+//                .allowCredentials(false);
+//    }
+//}
